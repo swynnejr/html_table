dd6032a34b39a7fa2ef690d4d5b38cc97d64de0e
swynnejr <saucersam@gmail.com>
2021-07-09 23:44:43 -0500

    for loop attempt

--- html_table.py
+++ html_table.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/lists')
-def render_lists():
+@app.route('/users')
+def render_lists(users):
     # Soon enough, we'll get data from a database, but for now, we're hard coding data
-    student_info = [
-        {'name' : 'Michael', 'age' : 35},
-        {'name' : 'John', 'age' : 30 },
-        {'name' : 'Mark', 'age' : 25},
-        {'name' : 'KB', 'age' : 27}
+    users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
+    return render_template("lists.html", users = users)
 
 
 
